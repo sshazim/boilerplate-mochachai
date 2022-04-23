81b1932b972bf08bf1b6a5e30df1fd46df77c3b8
sshazim <sshazim4u@gmail.com>
2022-04-23 11:26:39 +0530

    Upfated functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -98,17 +98,17 @@ suite('Functional Tests with Zombie.js', function () {
 
   suite('"Famous Italian Explorers" form', function () {
     
-//    test('Submit the surname "Polo" in the HTML form', function (done) {
-//      browser.fill('surname', 'Polo').then(() => {
-//        browser.pressButton('submit', () => {
-//         browser.assert.success();
-//          browser.assert.text('span#name', 'Marco');
-//          browser.assert.text('span#surname', 'Polo');
-//          browser.assert.elements('span#dates', 1);
-//          done();
-//        });
-//      });
-//    });
+    test('Submit the surname "Polo" in the HTML form', function (done) {
+      browser.fill('surname', 'Polo').then(() => {
+        browser.pressButton('submit', () => {
+         browser.assert.success();
+          browser.assert.text('span#name', 'Marco');
+          browser.assert.text('span#surname', 'Polo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
+    });
     
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
