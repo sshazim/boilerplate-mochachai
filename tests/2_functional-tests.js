dbbd868d9db807f2bd2ed4f487b713030fda4e4b
sshazim <sshazim4u@gmail.com>
2022-04-23 23:12:16 +0530

    Updated browser site

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -63,7 +63,7 @@ suite("Functional Tests", function () {
 
 const Browser = require("zombie");
 
- Browser.site='https://boilerplate-mochachai-2.shazimsurmawala.repl.co';
+ Browser.site='https://boilerplate-mochachai-1.shazimsurmawala.repl.co';
  
 suite("Functional Tests with Zombie.js", function () {
 const browser = new Browser();
