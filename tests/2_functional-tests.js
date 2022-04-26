707263010fc8dcd15303c67b924594f8827b0c61
sshazim <sshazim4u@gmail.com>
2022-04-26 21:36:59 +0530

    Updated funtional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -73,25 +73,25 @@ suiteSetup(function(done) {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      browser.fill('surname', 'Colombo').then(() => {
-        browser.pressButton('submit', () => {
+      browser.fill('surname', 'Colombo');
+      browser.pressButton('submit', function () {
           browser.assert.success();
           browser.assert.text('span#name', 'Cristoforo');
           browser.assert.text('span#surname', 'Colombo');
           browser.assert.elements('span#dates', 1);
           done();
         });
-      });
     });
+
       // #6
      test('Submit the surname "Vespucci" in the HTML form', function (done) {
        browser.fill('surname', 'Vespucci');
        browser.pressButton("submit",  ()=> {
-      browser.assert.success();
-       browser.assert.text('span#name', 'Amerigo');
-       browser.assert.text('span#surname', 'Vespucci');
-       browser.assert.element('span#dates', 1);
-       done();
+        browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.element('span#dates', 1);
+        done();
       });
     });
   });
